877aad11fb6f58e5ea582de2ab05a0181446949f
Mitchellhogue0 <mitchell.hogue0@gmail.com>
2021-08-13 15:21:40 -0500

    commits edit button

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -2,27 +2,30 @@ import createView from "../createView.js";
 
 export default function PostIndex(props) {
     return `
-        <header>
+         <header>
             <h1>Posts Page</h1>
         </header>
         <main>
-<!--        CREATE FORM         -->
-                    <form>
-                        <div>
-                            <div>
-                                <span>Post Title: </span>
-                            </div>
-                            <input id="create-post-title" type="text">
-                            <div>
-                                <span>Post Content: </span>
-                            </div>
-                            <input id="create-post-content" type="text">
-                        </div>
-                        <button id="create-btn">Submit</button>
-                    </form>
+            <!--        CREATE FORM         -->
+            <form>
+                <div class="post">
+                    <div>
+                        <span>Post Title: </span>
+                    </div>
+                    <input id="create-post-title" type="text">
+                    <div>
+                        <span>Post Content: </span>
+                    </div>
+                    <input id="create-post-content" type="text">
+                </div>
+                <button id="create-btn">Submit</button>
+            </form>
             <div class="post-container">
                 ${props.posts.map(post => `<h3 class="post-title" data-id="${post.id}">${post.title}</h3>
-                                            <h4>${post.content}</h4>`).join('')}   
+                <h4>${post.content}</h4>
+                <button class="edit-btn" data-id="${post.id}">edit</button>
+
+                `).join('')}
             </div>
         </main>
     `;
@@ -55,6 +58,18 @@ export function PostsEvent() {
             createView("/posts");
         });
     });
+}
 
 
+function postsEvent () {
+    //createEvent()
+    //editEvent()
+}
+
+function editEvent() {
+    $(".edit-btn").click(function (){
+        console.log("edit event fired off");
+        $(this).siblings(".edit-title, .edit-content");
+        $(this).text("Save");
+    })
 }
